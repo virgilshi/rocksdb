fcaa8fb914c0d8a1aa36c75b84ffa69a0b5e4f87
Jim Harris <james.r.harris@intel.com>
2016-06-13 13:08:50 -0700

    For setup script, cd into RESULTS_DIR for most of the operations.
    This simplifies script a bit by not requiring $RESULTS_DIR everywhere.
    
    Signed-off-by: Jim Harris <james.r.harris@intel.com>
    Change-Id: I12ed8e359b62b342f34dcd12f3d43467251e87af

--- setup.sh
+++ setup.sh
@@ -14,70 +14,72 @@ echo -n Building RocksDB...
 [ -e db_bench ] || make -j16 release &> $RESULTS_DIR/build_log.txt
 echo done.
 
+cd $RESULTS_DIR
+
 echo -n Creating and mounting XFS filesystem...
 sudo mkdir -p /mnt/rocksdb
 sudo umount /mnt/rocksdb || true
-sudo mkfs.xfs -d agcount=32 -l su=4096 -f /dev/nvme0n1 &> $RESULTS_DIR/mkfs_xfs.txt
+sudo mkfs.xfs -d agcount=32 -l su=4096 -f /dev/nvme0n1 &> mkfs_xfs.txt
 sudo mount /dev/nvme0n1 /mnt/rocksdb
 sudo chown $USER /mnt/rocksdb
 echo done.
 
-cp common_flags.txt $RESULTS_DIR/seq_insert_flags.txt
-echo "--benchmarks=fillseq" >> $RESULTS_DIR/seq_insert_flags.txt
-echo "--threads=1" >> $RESULTS_DIR/seq_insert_flags.txt
-echo "--disable_wal=1" >> $RESULTS_DIR/seq_insert_flags.txt
-echo "--use_existing_db=0" >> $RESULTS_DIR/seq_insert_flags.txt
+cp ../common_flags.txt seq_insert_flags.txt
+echo "--benchmarks=fillseq" >> seq_insert_flags.txt
+echo "--threads=1" >> seq_insert_flags.txt
+echo "--disable_wal=1" >> seq_insert_flags.txt
+echo "--use_existing_db=0" >> seq_insert_flags.txt
 
-cp common_flags.txt $RESULTS_DIR/overwrite_flags.txt
-echo "--benchmarks=overwrite" >> $RESULTS_DIR/overwrite_flags.txt
-echo "--threads=1" >> $RESULTS_DIR/overwrite_flags.txt
-echo "--duration=120" >> $RESULTS_DIR/overwrite_flags.txt
-echo "--disable_wal=0" >> $RESULTS_DIR/overwrite_flags.txt
-echo "--use_existing_db=1" >> $RESULTS_DIR/overwrite_flags.txt
+cp ../common_flags.txt overwrite_flags.txt
+echo "--benchmarks=overwrite" >> overwrite_flags.txt
+echo "--threads=1" >> overwrite_flags.txt
+echo "--duration=120" >> overwrite_flags.txt
+echo "--disable_wal=0" >> overwrite_flags.txt
+echo "--use_existing_db=1" >> overwrite_flags.txt
 
-cp common_flags.txt $RESULTS_DIR/readwrite_flags.txt
-echo "--benchmarks=readwhilewriting" >> $RESULTS_DIR/readwrite_flags.txt
-echo "--threads=4" >> $RESULTS_DIR/readwrite_flags.txt
-echo "--duration=120" >> $RESULTS_DIR/readwrite_flags.txt
-echo "--disable_wal=0" >> $RESULTS_DIR/readwrite_flags.txt
-echo "--use_existing_db=1" >> $RESULTS_DIR/readwrite_flags.txt
+cp ../common_flags.txt readwrite_flags.txt
+echo "--benchmarks=readwhilewriting" >> readwrite_flags.txt
+echo "--threads=4" >> readwrite_flags.txt
+echo "--duration=120" >> readwrite_flags.txt
+echo "--disable_wal=0" >> readwrite_flags.txt
+echo "--use_existing_db=1" >> readwrite_flags.txt
 
 echo -n Inserting keys and values into database...
-cat /sys/block/nvme0n1/stat > $RESULTS_DIR/blockdev_stats_insert.txt
-/usr/bin/time perf record ./db_bench --flagfile=$RESULTS_DIR/seq_insert_flags.txt &> $RESULTS_DIR/db_bench_insert.txt
-cat /sys/block/nvme0n1/stat >> $RESULTS_DIR/blockdev_stats_insert.txt
+cat /sys/block/nvme0n1/stat > blockdev_stats_insert.txt
+/usr/bin/time perf record ../db_bench --flagfile=seq_insert_flags.txt &> db_bench_insert.txt
+cat /sys/block/nvme0n1/stat >> blockdev_stats_insert.txt
 echo done.
 
 echo -n Generating perf report for insertion phase...
-mv perf.data $RESULTS_DIR/insert.perf.data
-sudo perf report -f -n -i $RESULTS_DIR/insert.perf.data | sed '/#/d' | sed '/%/!d' | sort -r > $RESULTS_DIR/insert.perf.txt
-rm $RESULTS_DIR/insert.perf.data
-./postprocess.py $RESULTS_DIR insert > $RESULTS_DIR/insert_summary.txt
+mv perf.data insert.perf.data
+sudo perf report -f -n -i insert.perf.data | sed '/#/d' | sed '/%/!d' | sort -r > insert.perf.txt
+rm insert.perf.data
+../postprocess.py `pwd` insert > insert_summary.txt
 echo done.
 
 echo -n Overwriting keys and values in database...
-cat /sys/block/nvme0n1/stat > $RESULTS_DIR/blockdev_stats_overwrite.txt
-/usr/bin/time perf record ./db_bench --flagfile=$RESULTS_DIR/overwrite_flags.txt &> $RESULTS_DIR/db_bench_overwrite.txt
-cat /sys/block/nvme0n1/stat >> $RESULTS_DIR/blockdev_stats_overwrite.txt
+cat /sys/block/nvme0n1/stat > blockdev_stats_overwrite.txt
+/usr/bin/time perf record ../db_bench --flagfile=overwrite_flags.txt &> db_bench_overwrite.txt
+cat /sys/block/nvme0n1/stat >> blockdev_stats_overwrite.txt
 echo done.
 
 echo -n Generating perf report for overwrite phase...
-mv perf.data $RESULTS_DIR/overwrite.perf.data
-sudo perf report -f -n -i $RESULTS_DIR/overwrite.perf.data | sed '/#/d' | sed '/%/!d' | sort -r > $RESULTS_DIR/overwrite.perf.txt
-rm $RESULTS_DIR/overwrite.perf.data
-./postprocess.py $RESULTS_DIR overwrite > $RESULTS_DIR/overwrite_summary.txt
+mv perf.data overwrite.perf.data
+sudo perf report -f -n -i overwrite.perf.data | sed '/#/d' | sed '/%/!d' | sort -r > overwrite.perf.txt
+rm overwrite.perf.data
+../postprocess.py `pwd` overwrite > overwrite_summary.txt
 echo done.
 
 echo -n Reading and overwriting keys and values in database...
-cat /sys/block/nvme0n1/stat > $RESULTS_DIR/blockdev_stats_readwrite.txt
-/usr/bin/time perf record ./db_bench --flagfile=$RESULTS_DIR/readwrite_flags.txt &> $RESULTS_DIR/db_bench_readwrite.txt
-cat /sys/block/nvme0n1/stat >> $RESULTS_DIR/blockdev_stats_readwrite.txt
+cat /sys/block/nvme0n1/stat > blockdev_stats_readwrite.txt
+/usr/bin/time perf record ../db_bench --flagfile=readwrite_flags.txt &> db_bench_readwrite.txt
+cat /sys/block/nvme0n1/stat >> blockdev_stats_readwrite.txt
 echo done.
 
 echo -n Generating perf report for read/write phase...
-mv perf.data $RESULTS_DIR/readwrite.perf.data
-sudo perf report -f -n -i $RESULTS_DIR/readwrite.perf.data | sed '/#/d' | sed '/%/!d' | sort -r > $RESULTS_DIR/readwrite.perf.txt
-rm $RESULTS_DIR/readwrite.perf.data
-./postprocess.py $RESULTS_DIR readwrite > $RESULTS_DIR/readwrite_summary.txt
+mv perf.data readwrite.perf.data
+sudo perf report -f -n -i readwrite.perf.data | sed '/#/d' | sed '/%/!d' | sort -r > readwrite.perf.txt
+rm readwrite.perf.data
+../postprocess.py `pwd` readwrite > readwrite_summary.txt
 echo done.
 
