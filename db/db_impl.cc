69835d927ec55df3dbe6ab54111fa833cf0ded13
Anirban Rahut <arahut@fb.com>
2016-08-25 15:17:52 -0700

    Creating a Blob Log reader writer from WAL reader/writer
    Summary: to get better control over the header and the blob
    log, replicating the code from WAL log reader/writer.
    
    Will have to figure out later, if this functionality can be merged
    
    Test Plan:
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Blame Revision:

--- db/db_impl.cc
+++ db/db_impl.cc
@@ -1098,6 +1098,7 @@ void DBImpl::PurgeObsoleteFiles(const JobContext& state, bool schedule_only) {
       case kIdentityFile:
       case kMetaDatabase:
       case kOptionsFile:
+      case kBlobFile:
         keep = true;
         break;
     }
