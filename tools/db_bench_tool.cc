23d5b8185ea92cbcea922b557da83ae4008e8dc0
Jim Harris <james.r.harris@intel.com>
2017-03-13 16:21:45 -0700

    Add SPDK BlobFS integration.
    This patches adds support for using the SPDK BlobFS as the
    storage engine for a RocksDB database.
    
    The Makefile changes assume that SPDK and DPDK have been
    built according to the SPDK README (https://github/spdk/spdk)
    and share the same parent directory as the RocksDB repository.
    
    Flags to configure the SPDK storage engine:
    
    spdk -
      Defines the name of the SPDK configuration file.  If
      omitted, will use PosixEnv instead of SpdkEnv.  This
      configuration file defines the block devices that may
      contain the BlobFS for the database.
    
    spdk_bdev -
      Defines the name of the SPDK block device which
      contains the BlobFS for this database.
    
    spdk_cache_size -
      Defines the amount of userspace cache memory used by
      BlobFS.  Specified in terms of megabytes (MB).  Default
      is 4096 (4GB).
    
    Change-Id: I72edabc27191492b1d1b6a3e314390c803251571
    Signed-off-by: Jim Harris <james.r.harris@intel.com>
    Signed-off-by: Ben Walker <benjamin.walker@intel.com>

--- tools/db_bench_tool.cc
+++ tools/db_bench_tool.cc
@@ -817,6 +817,10 @@ static bool ValidateTableCacheNumshardbits(const char* flagname,
 }
 DEFINE_int32(table_cache_numshardbits, 4, "");
 
+DEFINE_string(spdk, "", "Name of SPDK configuration file");
+DEFINE_string(spdk_bdev, "", "Name of SPDK blockdev to load");
+DEFINE_uint64(spdk_cache_size, 4096, "Size of SPDK filesystem cache (in MB)");
+
 #ifndef ROCKSDB_LITE
 DEFINE_string(env_uri, "", "URI for registry Env lookup. Mutually exclusive"
               " with --hdfs.");
@@ -5796,6 +5800,15 @@ int db_bench_tool(int argc, char** argv) {
     FLAGS_env  = new rocksdb::HdfsEnv(FLAGS_hdfs);
   }
 
+  if (!FLAGS_spdk.empty()) {
+    FLAGS_env = rocksdb::NewSpdkEnv(rocksdb::Env::Default(), FLAGS_db, FLAGS_spdk, FLAGS_spdk_bdev, FLAGS_spdk_cache_size);
+    if (FLAGS_env == NULL) {
+      fprintf(stderr, "Could not load SPDK blobfs - check that SPDK mkfs was run "
+                      "against block device %s.\n", FLAGS_spdk_bdev.c_str());
+      exit(1);
+    }
+  }
+
   if (!strcasecmp(FLAGS_compaction_fadvice.c_str(), "NONE"))
     FLAGS_compaction_fadvice_e = rocksdb::Options::NONE;
   else if (!strcasecmp(FLAGS_compaction_fadvice.c_str(), "NORMAL"))
@@ -5823,7 +5836,7 @@ int db_bench_tool(int argc, char** argv) {
   // Choose a location for the test database if none given with --db=<path>
   if (FLAGS_db.empty()) {
     std::string default_db_path;
-    rocksdb::Env::Default()->GetTestDirectory(&default_db_path);
+    FLAGS_env->GetTestDirectory(&default_db_path);
     default_db_path += "/dbbench";
     FLAGS_db = default_db_path;
   }
@@ -5834,8 +5847,15 @@ int db_bench_tool(int argc, char** argv) {
     FLAGS_stats_interval = 1000;
   }
 
-  rocksdb::Benchmark benchmark;
-  benchmark.Run();
+  // Allocate the Benchmark off the heap, so that we can explicitly delete it
+  //  before the SpdkEnv is deleted.
+  rocksdb::Benchmark *benchmark = new rocksdb::Benchmark;
+  benchmark->Run();
+  delete benchmark;
+
+  if (!FLAGS_spdk.empty()) {
+    delete FLAGS_env;
+  }
 
 #ifndef ROCKSDB_LITE
   if (FLAGS_print_malloc_stats) {
@@ -5844,7 +5864,6 @@ int db_bench_tool(int argc, char** argv) {
     fprintf(stdout, "Malloc stats:\n%s\n", stats_string.c_str());
   }
 #endif  // ROCKSDB_LITE
-
   return 0;
 }
 }  // namespace rocksdb
